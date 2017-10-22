ba7547ec82c74a8ac85135558ab3445298546db3
furion <_@furion.me>
2017-10-23 01:22:23 +0200

    remove dropzone.js, partially integrate FineUploader

--- README.md
+++ README.md
@@ -62,3 +62,60 @@ flask run --reload --debugger
 | UPLOADER_URI  | http://localhost:5005            |
 | MAIL_username | postmaster@mg.view.ly            |
 | MAIL_PASSWORD | ''                               |
+
+
+# Production
+
+## S3 Config
+This guide assumes the bucket has been created with proper IAM permissions.
+This guide uses bucket `flask-uploader-test` in `eu-eastern-1` region as example.
+
+**Environment Variables:**
+
+| Variable               | Default                   |
+| ---------------------- | ------------------------- |
+| S3_BUCKET              | flask-uploader-test       |
+| S3_REGION              | eu-central-1 eu-central-1 |
+| S3_UPLOADER_PUBLIC_KEY |                           |
+| S3_UPLOADER_SECRET_KEY |                           |
+
+## S3 IAM
+Create a `s3-fine-uploader` group with the following restricted S3 Policy:
+```json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::flask-uploader-test/*"
+        }
+    ]
+}
+```
+Assign a new user `s3-viewly-uploader` to it. 
+Use this users credentials as `S3_UPLOADER_PUBLIC_KEY` and `S3_UPLOADER_SECRET_KEY`.
+
+
+## S3 CORS Policy
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+<CORSRule>
+    <AllowedOrigin>*</AllowedOrigin>
+    <AllowedMethod>POST</AllowedMethod>
+    <AllowedMethod>PUT</AllowedMethod>
+    <AllowedMethod>DELETE</AllowedMethod>
+    <MaxAgeSeconds>3000</MaxAgeSeconds>
+    <ExposeHeader>ETag</ExposeHeader>
+    <AllowedHeader>content-type</AllowedHeader>
+    <AllowedHeader>origin</AllowedHeader>
+    <AllowedHeader>x-amz-acl</AllowedHeader>
+    <AllowedHeader>x-amz-meta-qqfilename</AllowedHeader>
+    <AllowedHeader>x-amz-date</AllowedHeader>
+    <AllowedHeader>x-amz-content-sha256</AllowedHeader>
+    <AllowedHeader>authorization</AllowedHeader>
+</CORSRule>
+</CORSConfiguration>
+```
+*TODO: Make `AllowedOrigin` more restrictive.*
--- src/b_upload/__init__.py
+++ src/b_upload/__init__.py
@@ -1,12 +1,15 @@
-import datetime as dt
+import base64
 import hashlib
 import hmac
+import os
 
+from boto.s3.connection import Key, S3Connection
 from flask import (
     Blueprint,
     render_template,
     request,
     make_response,
+    jsonify,
 )
 from flask_security import login_required
 from toolz import thread_first
@@ -17,48 +20,96 @@ upload = Blueprint(
     template_folder='templates'
 )
 
+# for s3 delete endpoint
+S3_MANAGER_PUBLIC_KEY = os.getenv('S3_MANAGER_PUBLIC_KEY')
+S3_MANAGER_SECRET_KEY = os.getenv('S3_MANAGER_SECRET_KEY')
 
-@upload.route('/')
+# for s3 upload signatures
+S3_UPLOADER_PUBLIC_KEY = os.getenv('S3_UPLOADER_PUBLIC_KEY')
+S3_UPLOADER_SECRET_KEY = os.getenv('S3_UPLOADER_SECRET_KEY')
+S3_BUCKET = os.getenv('S3_BUCKET', 'flask-uploader-test')
+S3_REGION = os.getenv('S3_REGION', 'eu-central-1')
+
+
+@upload.route('s3/sign', methods=['POST'])
 @login_required
-def index():
-    return render_template('index.html')
+def s3_signature():
+    policy = base64.b64encode(request.data)
+    conditions = request.get_json()['conditions']
 
+    def find(key):
+        from funcy import first
+        return first(x[key] for x in conditions if key in x)
 
-@upload.route('/s3_sign')
-@login_required
-def s3_sign():
-    to_sign = str(request.args.get('to_sign')).encode('utf-8')
-    date_stamp = dt.datetime.strptime(
-        request.args.get('datetime'),
-        '%Y%m%dT%H%M%SZ',
-    ).strftime('%Y%m%d')
-
-    aws_secret = 'AWS_SECRET_KEY'
-    region = 'AWS_REGION'
-    service = 's3'
+    amz_credential = find('x-amz-credential')
+    if not amz_credential:
+        return jsonify(invalid=True), 500
 
     # Key derivation functions. See:
     # http://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-python
     def sign(key, msg):
         return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
 
-    def get_signature(key, timestamp, region_name, service_name):
+    def calc_signature(secret_key, _, datestamp, region, service, aws_request):
+        # assert service == 's3'  # this shouldn't be a problem if AMI was setup right
         return thread_first(
-            f'AWS4{key}'.encode('utf-8'),
-            (sign, timestamp),
-            (sign, region_name),
-            (sign, service_name),
-            (sign, 'aws4_request'),
+            f'AWS4{secret_key}'.encode('utf-8'),
+            (sign, datestamp),
+            (sign, region),
+            (sign, service),
+            (sign, aws_request),
         )
 
-    signing_key = get_signature(aws_secret, date_stamp, region, service)
+    try:
+        signing_key = calc_signature(
+            S3_UPLOADER_SECRET_KEY,
+            *amz_credential.split('/')
+        )
+        signature = hmac.new(signing_key, policy, hashlib.sha256)
+    except:
+        return jsonify(
+            error="There was an error generating the AWS signature"), 500
 
-    signature = hmac.new(
-        signing_key,
-        to_sign,
-        hashlib.sha256
-    ).hexdigest()
+    return jsonify(
+        policy=policy.decode(),
+        signature=signature.hexdigest(),
+    )
 
-    resp = make_response(signature)
-    resp.headers['Content-Type'] = "text/plain"
-    return resp
+
+@upload.route("s3/delete/<key>", methods=['POST', 'DELETE'])
+@login_required
+def s3_delete(key=None):
+    """ Route for deleting files off S3. Uses the SDK. """
+    request_payload = request.values
+    key_name = request_payload.get('key')
+    bucket_name = request_payload.get('bucket')
+    assert bucket_name == S3_BUCKET
+
+    # TODO: check if this user is the owner of the file,
+    # and thus allowed to delete it
+
+    s3 = S3Connection(
+        S3_MANAGER_PUBLIC_KEY,
+        S3_MANAGER_SECRET_KEY
+    )
+    aws_bucket = s3.get_bucket(bucket_name, validate=False)
+    aws_key = Key(aws_bucket, key_name)
+    aws_key.delete()
+    return make_response('', 200)
+
+
+@upload.route("s3/success", methods=['GET', 'POST'])
+def s3_success():
+    # TODO: do something...
+    return make_response()
+
+
+@upload.route('/')
+@login_required
+def index():
+    return render_template(
+        'index.html',
+        s3_bucket_name=S3_BUCKET,
+        s3_bucket_region=S3_REGION,
+        s3_user_access_key=S3_UPLOADER_PUBLIC_KEY,
+    )
