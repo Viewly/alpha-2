bf7087996bde542728b0480ece1fc7776e2eb04d
furion <_@furion.me>
2018-02-25 18:57:11 +0100

    Optimize S3 Transfers

--- src/core/s3.py
+++ src/core/s3.py
@@ -1,6 +1,9 @@
 import io
 
 import boto3
+from botocore.config import Config as BotoConfig
+from boto3.s3.transfer import S3Transfer as BotoTransfer
+from boto3.s3.transfer import TransferConfig
 
 from ..config import (
     S3_UPLOADS_BUCKET,
@@ -11,26 +14,67 @@ from ..config import (
 
 
 class S3Transfer:
-    def __init__(self, region_name=None, bucket_name=None):
+    def __init__(
+            self,
+            region_name=None,
+            bucket_name=None,
+            accelerated_transfer=True,
+    ):
         self._region_name = region_name or S3_UPLOADS_REGION
         self._bucket_name = bucket_name or S3_UPLOADS_BUCKET
 
-        self.s3 = boto3.client(
+        self.client = boto3.client(
             's3',
             region_name=self._region_name,
             aws_access_key_id=AWS_MANAGER_PUBLIC_KEY,
             aws_secret_access_key=AWS_MANAGER_PRIVATE_KEY,
+            config=BotoConfig(s3={
+                "use_accelerate_endpoint": accelerated_transfer,
+            })
         )
 
+        self._config = TransferConfig(
+            multipart_threshold=25 * 1024 * 1024,  # 25 MB
+            max_concurrency=15,
+            num_download_attempts=5,
+            io_chunksize=1024 * 1024,  # 1 MB
+        )
+        self.transfer = BotoTransfer(self.client, self._config)
+
     def download_bytes(self, key: str) -> io.BytesIO:
-        response = self.s3.get_object(
+        response = self.client.get_object(
             Bucket=self._bucket_name,
             Key=key
         )
         return io.BytesIO(response['Body'].read())
 
+    def upload_fileobj(self, obj, key: str, overwrite=True):
+        # s3 weirdness prevention
+        key = key.lstrip('/')
+
+        # don't overwrite unless allowed
+        if self.key_exists(key) and not overwrite:
+            return
+
+        return self.client.upload_fileobj(
+            obj,
+            self._bucket_name,
+            key,
+        )
+
+    def key_exists(self, key):
+        try:
+            self.client.get_object_acl(
+                Bucket=self._bucket_name,
+                Key=key
+            )
+        except self.client.exceptions.NoSuchKey:
+            return False
+
+        return True
+
     def download_file(self, key: str, file_name: str):
-        return self.s3.download_file(
+        return self.transfer.download_file(
             self._bucket_name,
             key,
             file_name
@@ -44,33 +88,8 @@ class S3Transfer:
         if self.key_exists(key) and not overwrite:
             return
 
-        return self.s3.upload_file(
+        return self.transfer.upload_file(
             file_path,
             self._bucket_name,
             key,
         )
-
-    def upload_fileobj(self, obj, key: str, overwrite=True):
-        # s3 weirdness prevention
-        key = key.lstrip('/')
-
-        # don't overwrite unless allowed
-        if self.key_exists(key) and not overwrite:
-            return
-
-        return self.s3.upload_fileobj(
-            obj,
-            self._bucket_name,
-            key,
-        )
-
-    def key_exists(self, key):
-        try:
-            self.s3.get_object_acl(
-                Bucket=self._bucket_name,
-                Key=key
-            )
-        except self.s3.exceptions.NoSuchKey:
-            return False
-
-        return True
