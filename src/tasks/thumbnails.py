744f9acd03c314e3514d76662091d13d883d4de6
furion <_@furion.me>
2018-03-23 20:23:03 +0100

    Reject cover images below grid view size requirement

--- src/tasks/thumbnails.py
+++ src/tasks/thumbnails.py
@@ -17,6 +17,7 @@ from ..config import (
 from ..core.media import (
     img_resize_multi_to_s3,
     img_from_s3,
+    MinResNotAvailableError,
 )
 from ..models import Video
 
@@ -46,27 +47,32 @@ def process_thumbnails(video_id: str):
     )
 
     # resize into multiple sizes, compress
-    sizes = img_resize_multi_to_s3(
-        img,
-        output_key_prefix=f'thumbnails/{video.id}/',
-        region_name=S3_VIDEOS_REGION,
-        bucket_name=S3_VIDEOS_BUCKET,
-    )
-
-    # if thumbnails existed previously, invalidate CDN cache
-    thumbnail_files = video.file_mapper.thumbnail_files
-    if thumbnail_files:
-        invalidate_cdn_cache.delay(
-            s3_keys=[f"/{x}" for x in thumbnail_files.values()],
-            reference_id=str(time.time()),
+    try:
+        sizes = img_resize_multi_to_s3(
+            image=img,
+            min_size_name='small',
+            output_key_prefix=f'thumbnails/{video.id}/',
+            region_name=S3_VIDEOS_REGION,
+            bucket_name=S3_VIDEOS_BUCKET,
         )
+    except MinResNotAvailableError:
+        # todo, delete actual s3 file as well
+        video.file_mapper.s3_upload_thumbnail_key = None
+    else:
+        # if thumbnails existed previously, invalidate CDN cache
+        thumbnail_files = video.file_mapper.thumbnail_files
+        if thumbnail_files:
+            invalidate_cdn_cache.delay(
+                s3_keys=[f"/{x}" for x in thumbnail_files.values()],
+                reference_id=str(time.time()),
+            )
+        video.file_mapper.thumbnail_files = \
+            {x['name']: f"thumbnails/{video.id}/{x['file']}" for x in sizes}
 
-    video.file_mapper.thumbnail_files = \
-        {x['name']: f"thumbnails/{video.id}/{x['file']}" for x in sizes}
+        generate_manifest_file.delay(video.id)
+
     session.add(video)
     session.commit()
-
-    generate_manifest_file.delay(video.id)
 
 # THUMBNAIL PIPELINE
 # download the original
