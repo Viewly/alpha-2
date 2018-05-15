a16f2f52188e3d2906d10f524b1ea15155f96204
furion <_@furion.me>
2018-05-15 11:52:56 +0200

    Create a distribution game blueprint
    This blueprint is like Etherscan for the Distribution Game.

---
+++ src/b_game/__init__.py
@@ -0,0 +1,141 @@
+from flask import (
+    Blueprint,
+    render_template,
+)
+from sqlalchemy import desc, func
+
+from .. import db
+from ..models import (
+    Video,
+    Vote,
+    GamePeriod,
+    Reward,
+)
+
+game = Blueprint(
+    'game',
+    __name__,
+    template_folder='templates'
+)
+
+
+@game.route('/')
+def index():
+    return render_template('index.html')
+
+
+@game.route('/periods')
+def list_periods():
+    periods = \
+        (db.session.query(GamePeriod)
+         .order_by(desc(GamePeriod.end))
+         .limit(1000)
+         .all())
+
+    return render_template(
+        'periods.html',
+        periods=periods,
+    )
+
+
+@game.route('/rewards')
+def list_rewards():
+    rewards = \
+        (db.session.query(Reward)
+         .filter_by(creator_payable=True)
+         .order_by(desc(Reward.period_id))
+         .limit(1000)
+         .all())
+
+    return render_template(
+        'rewards.html',
+        rewards=rewards,
+    )
+
+
+@game.route('/period/<int:period_id>')
+def period_rewards(period_id):
+    period = db.session.query(GamePeriod).filter_by(id=period_id).one()
+
+    rewards_summary = \
+        (db.session.query(
+            Reward.video_id,
+            func.count(Reward.id),
+            func.sum(Reward.creator_reward).label('creator_rewards'),
+            func.sum(Reward.voter_reward))
+         .filter_by(period_id=period_id)
+         .group_by(Reward.video_id)
+         .order_by("creator_rewards desc")
+         .all())
+
+    rewards = \
+        (db.session.query(Reward, Vote)
+         .filter_by(period_id=period_id)
+         .from_self()
+         .join(Vote, Vote.id == Reward.vote_id)
+         .order_by(desc(Reward.creator_reward))
+         .all())
+
+    return render_template(
+        'period_rewards.html',
+        period=period,
+        rewards=rewards,
+        rewards_summary=rewards_summary,
+    )
+
+
+@game.route('/votes/<string:video_id>')
+def video_votes(video_id: str):
+    video = db.session.query(Video).filter_by(id=video_id).one()
+
+    votes = \
+        (db.session.query(Vote)
+         .filter_by(video_id=video_id)
+         .order_by(desc(Vote.token_amount))
+         .all())
+
+    rewards = \
+        (db.session.query(Reward, Vote)
+         .filter_by(video_id=video_id)
+         .from_self()
+         .join(Vote, Vote.video_id == Reward.video_id)
+         .order_by(desc(Reward.creator_reward))
+         .all())
+
+    period = None
+    summary = None
+    if rewards:
+        period_id = rewards[0][0].period_id
+        period = db.session.query(GamePeriod).filter_by(id=period_id).one()
+        summary = \
+            (db.session.query(
+                func.count(Reward.id).label('rewards_count'),
+                func.sum(Reward.creator_reward).label('creator_rewards'),
+                func.sum(Reward.voter_reward).label('voter_rewards'))
+             .filter_by(video_id=video_id, creator_payable=True)
+             .one())
+
+    return render_template(
+        'video_votes.html',
+        video=video,
+        votes=votes,
+        rewards=rewards,
+        period=period,
+        summary=summary,
+    )
+
+
+@game.route('/voter/<string:eth_address>')
+def voter_activity(eth_address: str):
+    votes = \
+        (db.session.query(Vote)
+         .filter_by(eth_address=eth_address)
+         .order_by(desc(Vote.created_at))
+         .limit(100)
+         .all())
+
+    return render_template(
+        'voter.html',
+        eth_address=eth_address,
+        votes=votes,
+    )
