ba9c8a967011ef572433d2f6a9cef3b9fcbb2a3a
Boris Vujicic <turshija@gmail.com>
2018-08-09 15:27:33 +0200

    Don't throw error when wallet_app_container is not available

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -6,15 +6,17 @@ import store from "./store/index";
 import App from "./App.js";
 
 const rootElement = document.getElementById("wallet_app_container");
-const config = {
-  apiUrl: rootElement.getAttribute('data-api-url'),
-  authUrl: rootElement.getAttribute('data-auth-url')
-}
+if (rootElement) {
+  const config = {
+    apiUrl: rootElement.getAttribute('data-api-url'),
+    authUrl: rootElement.getAttribute('data-auth-url')
+  }
 
-ReactDOM.render((
-  <Provider store={store}>
-    <BrowserRouter>
-      <App config={config} />
-    </BrowserRouter>
-  </Provider>), rootElement
-);
+  ReactDOM.render((
+    <Provider store={store}>
+      <BrowserRouter>
+        <App config={config} />
+      </BrowserRouter>
+    </Provider>), rootElement
+  );
+}
