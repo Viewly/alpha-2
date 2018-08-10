cd0e1803758988e85477729a47c982a2db0c39a8
Boris Vujicic <turshija@gmail.com>
2018-08-10 13:07:32 +0200

    Remove unused code from headerButton component

--- src/static/js/components/headerButton/index.js
+++ src/static/js/components/headerButton/index.js
@@ -1,21 +1,7 @@
 import React, { Component} from "react";
-import "./index.css";
 
 export default class HeaderButton extends Component {
-
-  // walletClick = () => {
-  //   const { history } = this.props;
-  //
-  //   console.log('propz', this.props);
-  //   window.location.href = '/wallet';
-  // }
-
   render() {
     return null;
-    // return(
-    //   <div className="App">
-    //     <p onClick={this.walletClick}>[Wallet]</p>
-    //   </div>
-    // );
   }
 }
