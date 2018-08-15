7b75eff5465c7b5dee49ee00bf9155135c1318cc
Boris Vujicic <turshija@gmail.com>
2018-08-15 19:48:10 +0200

    Implement a popup to confirm mnemonic has been saved

--- src/static/js/components/walletPage/generate/step2/index.js
+++ src/static/js/components/walletPage/generate/step2/index.js
@@ -1,13 +1,87 @@
 import React, { Component} from "react";
 import { Wallet } from 'ethers';
+import { arrayShuffle } from '../../../../utils';
 
 export default class GeneratorStep2 extends Component {
-  render() {
+  state = {
+    words: [1, 2, 3],
+    input0: '',
+    input1: '',
+    input2: '',
+    isOpen: false,
+  }
+
+  componentDidMount () {
+    this.modal = window.jQuery(this.ref).modal();
+    this.modal.modal({
+      inverted: true,
+      onHide: () => {
+        this.setState({ isOpen: false });
+      },
+      onShow: () => {
+        this.setState({ isOpen: true });
+      }
+    });
+  }
+
+  componentDidUpdate (prevProps, prevState) {
     const { wallet, changeStep } = this.props;
+    const words = wallet.mnemonic.split(' ');
+
+    if ( (this.state.input0 === words[this.state.words[0] - 1])
+      && (this.state.input1 === words[this.state.words[1] - 1])
+      && (this.state.input2 === words[this.state.words[2] - 1])) {
+
+      this.modalClose();
+      changeStep(3, wallet)
+    }
+  }
+
+  modalOpen = () => {
+    this.setState({ words: this.generateRandomNumbers() });
+    this.modal.modal('show');
+  }
+
+  modalClose = () => {
+    this.modal.modal('hide');
+  }
+
+  generateRandomNumbers = () => {
+    return arrayShuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]).splice(0, 3).sort(function(a,b) {return a - b});
+  }
+
+  render() {
+    const { wallet } = this.props;
+    const { isOpen } = this.state;
     const words = wallet.mnemonic.split(' ');
 
     return (
       <div className='ui padded segment'>
+
+        <div ref={(ref) => this.ref = ref} className='ui mini modal'>
+          <div className='header'>Confirm</div>
+          <div className='content'>
+            <div className='ui warning message'>
+              <p>We need to make sure you have written mnemonic words!</p>
+            </div>
+
+            <div className='ui form'>
+              {this.state.words.map((item, idx) => {
+                return (
+                  <div key={`form-items-${idx}-${item}`} className={`field ${this.state[`input${idx}`] === words[item - 1] ? 'disabled' : 'error'}`}>
+                    <label>Word #{item}</label>
+                    <input type='text' value={this.state[`input${idx}`]} onClick={() => this.className = 'lolz'} onChange={(e) => this.setState({ [`input${idx}`]: e.target.value })} disabled={ this.state[`input${idx}`] === words[item-1] } />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+
+          <div className="actions">
+            <div className="ui button" onClick={() => this.modalClose()}>Cancel</div>
+          </div>
+
+        </div>
 
         <div className="ui negative message">
           <div className="header">
@@ -19,11 +93,11 @@ export default class GeneratorStep2 extends Component {
           </ul>
         </div>
 
-        <ol>
-          {words.map((word, idx) => <li key={`word-${word}-${idx}`}><strong>{word}</strong></li>)}
+        <ol style={{ columns: 2, columnGap: '60px' }}>
+          {words.map((word, idx) => <li key={`word-${word}-${idx}`}><strong>{!isOpen ? word : '*'.repeat(word.length)}</strong></li>)}
         </ol>
 
-        <button className='ui right labeled icon button primary' onClick={() => changeStep(3, wallet)}><i className="right arrow icon"></i> Next</button>
+        <button className='ui right labeled icon button primary' onClick={() => this.modalOpen()}><i className="right arrow icon"></i> Next</button>
       </div>
     )
   }
--- src/static/js/utils.js
+++ src/static/js/utils.js
@@ -62,6 +62,28 @@ export function getFirstWallet(wallets) {
   return { ...wallets[address], address };
 }
 
+/**
+ * @source https://stackoverflow.com/a/2450976
+ */
+export function arrayShuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 export function roundTwoDecimals(number) {
   return Math.round(number * 100)/100;
 }
