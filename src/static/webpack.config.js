efb39fb3b5d15fe210e79b8c6041c3f44541e045
Boris Vujicic <turshija@gmail.com>
2018-08-20 16:29:39 +0200

    Added svg-url-loader to webpack

--- src/static/webpack.config.js
+++ src/static/webpack.config.js
@@ -15,6 +15,13 @@ module.exports = {
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {}
+        }
       }
     ]
   },
