aeb328d3908a6560355d5666cdab0c8e7180832b
Boris Vujicic <turshija@gmail.com>
2018-08-23 14:25:48 +0200

    Check if element exist before rendering React app

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -5,10 +5,12 @@ import { Provider } from "react-redux";
 import store from "./store/index";
 import App from "./App.js";
 
-ReactDOM.render((
+const $appContainer = document.getElementById("wallet_app_container");
+
+$appContainer && ReactDOM.render((
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>), document.getElementById("wallet_app_container")
+  </Provider>), $appContainer
 );
