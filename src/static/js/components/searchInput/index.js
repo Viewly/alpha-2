6a1cf008cb483a0f5b48616681c57153a4d3905e
Boris Vujicic <turshija@gmail.com>
2018-09-10 15:30:49 +0200

    Implement functional keyboard shortcuts for dropdown (esc, up, down, enter)

--- src/static/js/components/searchInput/index.js
+++ src/static/js/components/searchInput/index.js
@@ -14,7 +14,9 @@ const MIN_SEARCH_CHARACTERS = 3;
 }))
 export default class SearchInput extends Component {
   state = {
-    searchText: ''
+    searchText: '',
+    selected: -1,
+    dropdownOpen: false,
   }
 
   componentDidMount() {
@@ -26,14 +28,43 @@ export default class SearchInput extends Component {
     const { doSearch } = this.props;
 
     if (prevState.searchText !== this.state.searchText) {
+      this.setState({ selected: -1, dropdownOpen: this.state.searchText.length >= MIN_SEARCH_CHARACTERS });
       doSearch(this.state.searchText);
     }
   }
 
   onKeyDown = (e) => {
-    if (e.key === 'Enter') {
-      window.location = "/search?q=" + this.state.searchText;
+    const { search: { data } } = this.props;
+
+    switch (e.key) {
+      case 'Enter':
+        this.setState({ dropdownOpen: false });
+        window.location = (this.state.selected === -1)
+          ? "/search?q=" + this.state.searchText
+          : data[this.state.selected].channel_url;
+        break;
+
+      case 'Escape':
+        this.onBlur();
+        break;
+
+      case 'ArrowUp':
+        this.setState({ selected: (this.state.selected === -1) ? data.length - 1 : this.state.selected - 1 });
+        break;
+
+      case 'ArrowDown':
+        this.setState({ selected: (this.state.selected >= data.length - 1) ? -1 : this.state.selected + 1});
+        break;
     }
+  }
+
+  onBlur = () => {
+    setTimeout(() => this.setState({ dropdownOpen: false, selected: -1 }), 100);
+  }
+
+  onFocus = (e) => {
+    this.setState({ dropdownOpen: this.state.searchText.length >= MIN_SEARCH_CHARACTERS });
+    e.target.select();
   }
 
   render() {
@@ -52,10 +83,19 @@ export default class SearchInput extends Component {
               </g>
             </svg>
           </button>
-          <input className="c-header__search__input" placeholder="Search" value={this.state.searchText} onChange={(e) => this.setState({ searchText: e.target.value })} onKeyDown={this.onKeyDown} />
+
+          <input
+            className="c-header__search__input"
+            placeholder={this.state.selected === -1 ? 'Search' : search.data[this.state.selected].display_name}
+            value={this.state.selected === -1 ? this.state.searchText : ''}
+            onChange={(e) => this.setState({ searchText: e.target.value })}
+            onBlur={this.onBlur}
+            onFocus={this.onFocus}
+            onKeyDown={this.onKeyDown}
+          />
         </div>
 
-        {this.state.searchText.length >= MIN_SEARCH_CHARACTERS && (
+        {this.state.dropdownOpen && (
           <div className="c-header-dropdown">
 
             <div className="ui middle aligned selection list">
@@ -68,8 +108,8 @@ export default class SearchInput extends Component {
                 </div>
               )}
 
-              {search.data.map(item => (
-                <a href={item.channel_url} className="item" key={`channel-${item.channel_id}`}>
+              {search.data.map((item, idx) => (
+                <a href={item.channel_url} className={`item ${this.state.selected === idx ? 'active' : ''}`} key={`channel-${item.channel_id}`}>
                   <img className="ui avatar image" src={item.avatar_url} onError={(e) => e.target.src = 'https://i.imgur.com/32AwiVw.jpg' } />
                   <div className="content">
                     <div className="header">{item.display_name}</div>
