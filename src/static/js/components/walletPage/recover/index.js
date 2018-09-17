97e494b64a081eb75cefe59290c1b446fc00cb6d
Boris Vujicic <turshija@gmail.com>
2018-09-17 18:09:27 +0200

    Implement generating new encrypted wallet and proof of owning privateKey WIP

--- src/static/js/components/walletPage/recover/index.js
+++ src/static/js/components/walletPage/recover/index.js
@@ -1,7 +1,10 @@
 import React, { Component} from "react";
 import { connect } from "react-redux";
 import { Wallet } from 'ethers';
+
+import Strength from '../generate/step3/strength';
 import { unlockWallet } from '../../../actions';
+import { updateWallets } from '../../../utils';
 
 @connect((state, props) => ({
   wallet: state.wallet.address === props.match.params.wallet && state.wallet,
@@ -11,12 +14,15 @@ import { unlockWallet } from '../../../actions';
 export default class WalletRecover extends Component {
   state = {
     mnemonic: '',
+    password: '',
+    progress: 0,
+    encrypting: false,
     error: false,
     errorText: ''
   }
 
   checkMnemonic = () => {
-    const { wallet, unlockWallet, history } = this.props;
+    const { wallet, unlockWallet } = this.props;
 
     this.setState({ error: false });
     try {
@@ -27,12 +33,53 @@ export default class WalletRecover extends Component {
         return false;
       }
 
+      updateWallets(newWallet);
       unlockWallet(newWallet.address, newWallet.privateKey);
-      history.push(`/wallet/${newWallet.address}`);
-
     } catch (e) {
       this.setState({ error: true, errorText: 'Invalid mnemonic'});
     }
+  }
+
+  signMessage = () => {
+    const { wallet } = this.props;
+
+    const message = "Viewly Wallet Recovery";
+    const tmpWallet = new Wallet(wallet.privateKey);
+    const signature = tmpWallet.signMessage(message);
+
+    return JSON.stringify({
+      "address": wallet.address,
+      "msg": message,
+      "sig": signature,
+      "version": "2"
+    })
+  }
+
+  encryptAndSave = () => {
+    const { wallet } = this.props;
+
+    const tmpWallet = new Wallet(wallet.privateKey);
+
+    this.setState({ error: false });
+    if ((!this.state.password) || (this.state.password.length > 64)) {
+      this.setState({ error: true });
+      return false;
+    }
+
+    this.setState({ encrypting: true });
+
+    var encryptPromise = tmpWallet.encrypt(this.state.password, (progress) => {
+      this.setState({ progress });
+    });
+
+    encryptPromise.then((json) => {
+      const payload = {
+        data: json,
+        signature: this.signMessage()
+      };
+
+      console.log('save payload', payload);
+    });
   }
 
   render() {
@@ -47,19 +94,42 @@ export default class WalletRecover extends Component {
           <div className="sub header">{wallet.address}</div>
         </h2>
 
-        <div className='ui padded container'>
-          <div className='ui message'>
-            Recover wallet using 12 word mnemonic
-          </div>
+        {!wallet.decrypted && (
+          <div className='ui padded container'>
+            <div className='ui message'>
+              Recover wallet using 12 word mnemonic
+            </div>
 
-          <div className={`ui fluid action input ${this.state.error && 'error'}`}>
-            <input type="text" placeholder="Your mnemonic here ..." value={this.state.mnemonic} onChange={(e) => this.setState({ mnemonic: e.target.value })} />
-            <div className="ui button" onClick={this.checkMnemonic}>Check</div>
+            <div className={`ui fluid action input ${this.state.error && 'error'}`}>
+              <input type="text" placeholder="Your mnemonic here ..." value={this.state.mnemonic} onChange={(e) => this.setState({ mnemonic: e.target.value })} />
+              <div className="ui button" onClick={this.checkMnemonic}>Unlock</div>
+            </div>
+            {this.state.error && (
+              <div className="ui pointing red label">{this.state.errorText}</div>
+            )}
           </div>
-          {this.state.error && (
-            <div className="ui pointing red label">{this.state.errorText}</div>
-          )}
-        </div>
+        )}
+        {wallet.decrypted && (
+          <div className='ui padded container'>
+            <div className='ui positive message'>
+              Wallet successfully unlocked !<br />
+              You can change password now using the input below.
+            </div>
+
+            {this.state.encrypting && <h3 className='ui header'>Encrypting wallet: {Math.round(this.state.progress * 100)}%</h3>}
+            {!this.state.encrypting && (
+              <div>
+                <div className="ui fluid action input">
+                  <input type="password" placeholder="New password" value={this.state.password} onChange={(e) => this.setState({ password: e.target.value })} />
+                  <div className="ui button" onClick={this.encryptAndSave}>Save password</div>
+                </div>
+                <div className="eight wide column">
+                  {this.state.password && <Strength password={this.state.password} />}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     )
   }
