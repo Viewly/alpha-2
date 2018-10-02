df6e22cc1ba5c178004e4a310854dbd8bec8faac
Boris Vujicic <turshija@gmail.com>
2018-10-02 18:25:08 +0200

    Finalize webpack configuration

--- src/static/webpack.config.js
+++ src/static/webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: ['babel-polyfill', './js/index.js', './scss/base.scss', './scss/http-status.scss'],
+  entry: ['babel-polyfill', './js/index.js', './scss/base.scss'],
   mode: "development",
   module: {
     rules: [
@@ -27,7 +27,7 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          // "style-loader", // creates style nodes from JS strings
+          // "style-loader", // creates style nodes from JS strings - has huge FOUC effect so I disabled it
           MiniCssExtractPlugin.loader,
           "css-loader?-url", // translates CSS into CommonJS
           "sass-loader" // compiles Sass to CSS, using Node Sass by default
